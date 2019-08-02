c6c5e7d2df105a2c7e1ddf18bc0f744582ad5bee
Mantvydas Binderis <mbdeveloping@gmail.com>
2019-08-02 11:15:12 +0100

    respawn both cars on lvl1

--- src/es/World.js
+++ src/es/World.js
@@ -5,45 +5,60 @@ import Road from './Road';
 export default class World {
     constructor() {
         this.speed = 7,
-        // this.randomSpeed = Math.floor(Math.random() * 2) + 1,
         this.playerCar = new Car(80, display.height - 200, 60, 100, 'black'),
         this.spawnPos = [
-            {x: 0, y: 0},
-            {x: 70, y: 0},
-            {x: 130, y: 0},
-            {x: 170, y: 0},
-            {x: 240, y: 0}
+            {x: 0, y: -100},
+            {x: 70, y: -100},
+            {x: 170, y: -100},
+            {x: 240, y: -100}
         ],
         this.npcCars = ["red", "green", "yellow"],
         this.npc = [],
         this.level = 1,
-        this.road = new Road(display, this.speed);
+        this.road = new Road(display, this.speed),
+        this.frameCounter = 0
     }
 
     createNpc() {
         if (this.npc.length <= this.level) {
-            this.npc.push(new Car(
-                this.spawnPos[Math.floor(Math.random() * this.spawnPos.length)].x,
-                this.spawnPos[0].y,
-                60,
-                100,
-                this.npcCars[0]
-            ));
+            for (let i = 0; i < 2; i++) {
+                this.npc.push(new Car(
+                    this.spawnPos[Math.floor(Math.random() * this.spawnPos.length)].x,
+                    this.spawnPos[0].y,
+                    60,
+                    100,
+                    this.npcCars[Math.floor(Math.random() * this.npcCars.length)]
+                ));
+            }
         }
     }
 
     showNpc() {
-        this.npc[0].show();
+        for (let i = 0; i < this.npc.length; i++) {
+            this.npc[i].show();
+        }
     }
 
     moveNpc() {
-        this.npc[0].pos.y += this.speed * 0.5;
+        if (this.level === 1) {
+            this.npc[0].pos.y += this.speed * 0.5;
+
+            if (this.frameCounter > 60) {
+                this.npc[1].pos.y += this.speed * 0.5;
+            }
+        }
     }
 
     restoreNpc() {
-        if (this.npc[0].pos.y >= display.height) {
-            this.npc[0].pos.y = -200;
-            this.npc[0].pos.x = this.spawnPos[Math.floor(Math.random() * this.spawnPos.length)].x;
+        for (let i = 0; i < this.npc.length; i++) {
+            if (this.npc[i].pos.y >= display.height) {
+                this.npc[i].pos.y = this.spawnPos[0].y; //all has same spawn pos
+                this.npc[i].pos.x = this.spawnPos[Math.floor(Math.random() * this.spawnPos.length)].x;
+            }
+
+            if (this.npc[1].pos.y >= display.height) {
+                this.frameCounter = 0;
+            }
         }
     }
 
@@ -59,6 +74,10 @@ export default class World {
         }
     }
 
+    countFrame() {
+        this.frameCounter++;
+    }
+
     update() {
         this.road.moveLines();
         this.road.restoreLines();
@@ -67,6 +86,7 @@ export default class World {
         this.createNpc();
         this.moveNpc();
         this.restoreNpc();
+        this.countFrame();
     }
 
     show() {
