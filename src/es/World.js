ccd8e8755c0a81fb3a5f52ef8e60d01a3ca67e89
Mantvydas <mbindeirs@gmail.com>
2019-08-04 13:54:08 +0100

    check if spawn pos is already used

--- src/es/World.js
+++ src/es/World.js
@@ -13,32 +13,45 @@ export default class World {
             {isUsed: false, x: 170, y: -100},
             {isUsed: false, x: 240, y: -100}
         ],
+        this.spawnCounter = 0,
         this.npcCars = ["red", "green", "yellow"],
         this.npc = [],
-        this.level = {
-            cars: 2,
-            // gameSpeed = 7
-        },
+        this.level = [
+            {cars: 2}
+        ],
         this.road = new Road(display, this.speed),
         this.frameCounter = 0
     }
 
     createNpc() {
-        if (this.npc.length <= this.level.cars) {
-            for (let i = 0; i < 2; i++) {
-                let random = Math.floor(Math.random() * this.spawnPos.length);
-        
-                if (this.spawnPos[random].isUsed !== true) {
-                    this.spawnPos[random].isUsed = true;
-                    this.npc.push(new Car(
-                        this.spawnPos[random].x,
-                        this.spawnPos[0].y,
-                        60,
-                        100,
-                        this.npcCars[Math.floor(Math.random() * this.npcCars.length)]
-                    ));
+        if (this.npc.length < this.level[0].cars) {
+            let randomPos = Math.floor(Math.random() * (this.spawnPos.length));
+
+            if (!this.spawnPos[randomPos].isUsed) {
+                this.spawnPos[randomPos].isUsed = true;
+                this.spawnCounter = randomPos;
+
+                this.npc.push(new Car(
+                    this.spawnPos[randomPos].x,
+                    this.spawnPos[0].y,
+                    60,
+                    100,
+                    this.npcCars[Math.floor(Math.random() * (this.npcCars.length - 1))]
+                ));
+            } else {
+                if (this.spawnCounter === this.spawnPos.length - 1) {
+                    this.spawnCounter = 0;
                 } else {
+                    this.spawnCounter += 1;
                 }
+
+                this.npc.push(new Car(
+                    this.spawnPos[this.spawnCounter].x,
+                    this.spawnPos[0].y,
+                    60,
+                    100,
+                    this.npcCars[Math.floor(Math.random() * (this.npcCars.length - 1))]
+                ));
             }
         }
     }
@@ -50,30 +63,31 @@ export default class World {
     }
 
     moveNpc() {
-        if (this.level === 1) {
-            this.npc[0].pos.y += this.speed * 0.5;
+        this.npc[0].pos.y += this.speed * 0.5;
 
-            if (this.frameCounter > 60) {
-                if (this.npc[1].acc === 0) {
-                    this.npc[1].acc = Math.random().toFixed(1);
-                }
-                this.npc[1].pos.y += this.speed * this.npc[1].acc;
+        if (this.frameCounter > 0) {
+            if (this.npc[1].acc === 0) {
+                this.npc[1].acc = Math.random().toFixed(1);
             }
+            this.npc[1].pos.y += this.speed * this.npc[1].acc;
         }
+
+        // for (let i = 0; i < this.npc.length; i++) {
+        //     this.npc[i].show();
+        // }
     }
 
     restoreNpc() {
         for (let i = 0; i < this.npc.length; i++) {
             if (this.npc[i].pos.y >= display.height) {
                 this.npc[i].pos.y = this.spawnPos[0].y; //all has same spawn pos
-                this.npc[i].pos.x = this.spawnPos[Math.floor(Math.random() * this.spawnPos.length)].x;
+                this.npc[i].pos.x = this.spawnPos[Math.floor(Math.random() * (this.spawnPos.length - 1))].x;
                 this.npc[i].acc = 0; //restore acc
             }
-
-            if (this.npc[1].pos.y >= display.height) {
-                this.frameCounter = 0;
-            }
         }
+        // if (this.npc[1].pos.y >= display.height) {
+        //     this.frameCounter = 0;
+        // }
     }
 
     updateCarPos() {
@@ -108,6 +122,7 @@ export default class World {
         this.road.createLines();
         this.playerCar.show();
         this.showNpc();
-        console.log(this.npc);
+        // console.log(this.npc);
+        // console.log(this.frameCounter);
     }
 }
